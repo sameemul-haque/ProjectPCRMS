4b6f2e5dd35be221f132703b9f2dc5026db78436
sameemul-haque <samhaqk@gmail.com>
2023-09-22 20:21:10 +0530

    changed style from table to card

--- public/police/view.js
+++ public/police/view.js
@@ -2,41 +2,36 @@ const db = firebase.firestore();
 
 const detailsList = document.getElementById("details-list");
 
-// Create a function to render detailss
-function renderDetails(doc) {
-  let tr = document.createElement("tr");
-  let policeIDTd = document.createElement("td");
-  let policeNameTd = document.createElement("td");
-  let policeStationTd = document.createElement("td");
-  let policeRankTd = document.createElement("td");
-  let policeContactTd = document.createElement("td");
+function renderCard(doc) {
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-  policeIDTd.textContent = doc.data().policeID;
-  policeNameTd.textContent = doc.data().policeName;
-  policeStationTd.textContent = doc.data().policeStation;
-  policeRankTd.textContent = doc.data().policeRank;
-  policeContactTd.textContent = doc.data().policeContact;
+  const policeID = doc.data().policeID;
+  const policeName = doc.data().policeName;
+  const policeStation = doc.data().policeStation;
+  const policeRank = doc.data().policeRank;
+  const policeContact = doc.data().policeContact;
 
+  card.innerHTML = `
+    <p><b>Police ID:</b> ${policeID}</p>
+    <p><b>Name:</b> ${policeName}</p>
+    <p><b>Police Station:</b> ${policeStation}</p>
+    <p><b>Rank:</b> ${policeRank}</p>
+    <p><b>Contact:</b> ${policeContact}</p>
+  `;
 
-  tr.appendChild(policeIDTd);
-  tr.appendChild(policeNameTd);
-  tr.appendChild(policeStationTd);
-  tr.appendChild(policeRankTd);
-  tr.appendChild(policeContactTd);
-
-  detailsList.appendChild(tr);
+  detailsList.appendChild(card);
 }
 
-// Get detailss from Firestore database
+// Get data from Firestore database
 db.collection("police")
   .get()
   .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      // Render each details
-      renderDetails(doc);
+      renderCard(doc);
     });
   })
   .catch((error) => {
-    console.error("Error getting details: ", error);
-    alert("Error getting details, please try again.");
+    console.error("Error getting data: ", error);
+    alert("Error getting data, please try again.");
   });
