0d7c7e101512aeec04a312f8180971d86cff0bc9
sameemul-haque <samhaqk@gmail.com>
2023-09-25 20:47:16 +0530

    added github issues link

--- public/login.js
+++ public/login.js
@@ -1,25 +1,29 @@
-// Get the login form and error message element
 const loginForm = document.querySelector('form');
 const errorMsg = document.getElementById('error-msg');
 
-// Add event listener to the login form
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  // Get the email and password input fields
   const email = loginForm.email.value;
   const password = loginForm.password.value;
 
-  // Use Firebase authentication to sign in the user
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      // Clear the form and error message
       loginForm.reset();
       errorMsg.innerText = '';
       window.location.href = '/pcrms';
     })
     .catch((error) => {
-      // Handle errors by displaying an error message to the user
       errorMsg.innerText = error.message;
     });
 });
+
+// let keysPressed = '';
+// document.body.addEventListener('keydown', function(event) {
+//   keysPressed += event.key.toLowerCase(); 
+//   if (keysPressed === 'help') {
+//     window.location.href = "https://github.com/sameemul-haque/projectpcrms";
+//   } else if (!'help'.startsWith(keysPressed)) {
+//     keysPressed = '';
+//   }
+// });
